9f87d061be6f4cc29e9b3126be33dc26b390df2a
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-11-01 23:38:27 +0100

    changed some issues

--- unidad_19_4/src/unidad_19_4/vista1.java
+++ unidad_19_4/src/unidad_19_4/vista1.java
@@ -16,7 +16,6 @@ import java.awt.event.ActionEvent;
 
 public class vista1 extends JFrame {
 
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	JLabel resultado = new JLabel("0");
